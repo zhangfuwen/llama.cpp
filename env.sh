621d1227c6e193f2cb169d46ed239ca398d9d71f
zhangfuwen <zhangfuwen@foxmail.com>
2024-03-06 16:19:31 +0800

    temp

---
+++ env.sh
@@ -0,0 +1,10 @@
+vcpkg_toolchain_file=/home/zhangfuwen/.clion-vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake
+android_toolchain_file=/home/zhangfuwen/bin/android-ndk-r25c/build/cmake/android.toolchain.cmake
+android_abi=arm64-v8a
+vcpkg_target_triplet=arm64-android
+
+
+CMAKE_TOOLCHAIN_FILE=/home/zhangfuwen/.clion-vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake
+DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=/home/zhangfuwen/bin/android-ndk-r25c/build/cmake/android.toolchain.cmake
+ANDROID_ABI=arm64-v8a
+VCPKG_TARGET_TRIPLET=arm64-android
